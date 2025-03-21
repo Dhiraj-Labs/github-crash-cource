b219cc4a7136d26acecfad7fb6e4a5c017b0b18a
dhga1121 <dhiraj.gaikwad@netcracker.com>
2025-03-21 21:33:11 +0530

    6th commit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const age = 24;
 const name = 'Dhiraj Gaikwad'
-<<<<<<< HEAD
 const dob = '24-apr-1990'
 
 function isAbove18(){
@@ -10,6 +9,6 @@ function isAbove18(){
 #function isAbove60(){
  #  return age=>60
 #}
-=======
 const dob = '24-apr-1990'
->>>>>>> parent of 14dceff ( 2nd commit)
+
+const age = 30; 
